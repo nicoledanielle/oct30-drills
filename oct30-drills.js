eeffc294923cf1cde27c87c676b6404ee1e0a7c8
Elliot Kim <elliotkim916@gmail.com>
2017-10-30 14:03:20 -0700

    added NaN conditional

--- oct30-drills.js
+++ oct30-drills.js
@@ -4,8 +4,16 @@ function yearOfBirth(age){
 
 function whoAmI(name,age) {   
     let yob = yearOfBirth(age);
+    if (isNaN(age) === false && isNaN(name) === true) {
     console.log("Hi, my name is " + name + " and I am " + age + " years old");
     console.log('I was born in ' + yob);
+    } else if (isNaN(age) === true || isNaN(name) === false) {
+        console.log('Error, try again!');
+    }
+
+    if (name === null || age === null) {
+        console.error('Arguments not valid');
+    } 
 
     try {
         if (age < 0) throw "is too low";
@@ -13,18 +21,11 @@ function whoAmI(name,age) {
     catch(err) {
         console.log('Age cannot be negative');
     }
-
-    if(name == null){
-        console.error("Arguments not valid")
-    }
-
-    if(age == null){
-        console.error("Arguments not valid")
-    }
+ 
 }
 
-yearOfBirth("29");
-whoAmI("Chris","29");
+yearOfBirth(29);
+whoAmI("Chris", 29);
 
 
 
