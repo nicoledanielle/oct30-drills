7db36a5a46d7b9562d6357949dfdc7742e932ef3
Elliot Kim <elliotkim916@gmail.com>
2017-10-30 13:32:36 -0700

    added try catch

--- oct30-drills.js
+++ oct30-drills.js
@@ -1,13 +1,23 @@
 function yearOfBirth(age){
-    return(2017 - age);
+    return(2017 - age); 
 }
 
 function whoAmI(name,age) {   
     let yob = yearOfBirth(age);
     console.log("Hi, my name is " + name + " and I am " + age + " years old");
     console.log('I was born in ' + yob);
+
+    try {
+        if (age < 0) throw "is too low";
+    }
+    
+    catch(err) {
+        console.log('Age cannot be negative');
+    }
     }
 
 yearOfBirth("29");
-
 whoAmI("Chris","29");
+
+
+
